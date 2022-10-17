a503c022b3a6b9827c69506102ac9bb4395da617
Serendipper-1 <90274855+Serendipper-1@users.noreply.github.com>
2022-10-17 19:02:20 +0800

    Add files via upload

---
+++ backend/ql.js
@@ -0,0 +1,210 @@
+'use strict';
+
+const got = require('got');
+require('dotenv').config();
+const { readFile } = require('fs/promises');
+const path = require('path');
+
+const qlDir = process.env.QL_DIR || '/ql/data';
+const authFile = path.join(qlDir, 'data/config/auth.json');
+
+const api = got.extend({
+  prefixUrl: process.env.QL_URL || 'http://localhost:5700',
+  retry: { limit: 0 },
+});
+
+async function getToken() {
+  const authConfig = JSON.parse(await readFile(authFile));
+  return authConfig.token;
+}
+
+module.exports.getEnvs = async () => {
+  const token = await getToken();
+  const body = await api({
+    url: 'api/envs',
+    searchParams: {
+      searchValue: 'JD_COOKIE',
+      t: Date.now(),
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  }).json();
+  return body.data;
+};
+
+module.exports.getEnvsCount = async () => {
+  const data = await this.getEnvs();
+  return data.length;
+};
+
+module.exports.addEnv = async (cookie, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'post',
+    url: 'api/envs',
+    params: { t: Date.now() },
+    json: [{
+      name: 'JD_COOKIE',
+      value: cookie,
+      remarks,
+    }],
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.updateEnv = async (cookie, eid, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: 'api/envs',
+    params: { t: Date.now() },
+    json: {
+      name: 'JD_COOKIE',
+      value: cookie,
+      id: eid,
+      remarks,
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.enableEnv = async (eid) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: 'api/envs/enable',
+    params: {t: Date.now()},
+    body: JSON.stringify([eid]),
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.disableEnv = async (eid) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: 'api/envs/disable',
+    params: {t: Date.now()},
+    body: JSON.stringify([eid]),
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+module.exports.delEnv = async (eid) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'delete',
+    url: 'api/envs',
+    params: { t: Date.now() },
+    body: JSON.stringify([eid]),
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+//////////////////////////////////////////////////
+// wskey
+module.exports.getWSCKEnvs = async () => {
+  const token = await getToken();
+  const body = await api({
+    url: 'api/envs',
+    searchParams: {
+      searchValue: 'JD_WSCK',
+      t: Date.now(),
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  }).json();
+  return body.data;
+};
+
+module.exports.getWSCKEnvsCount = async () => {
+  const data = await this.getWSCKEnvs();
+  return data.length;
+};
+
+module.exports.addWSCKEnv = async (jdwsck, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'post',
+    url: 'api/envs',
+    params: { t: Date.now() },
+    json: [{
+      name: 'JD_WSCK',
+      value: jdwsck,
+      remarks,
+    }],
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.updateWSCKEnv = async (jdwsck, wseid, remarks) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'put',
+    url: 'api/envs',
+    params: { t: Date.now() },
+    json: {
+      name: 'JD_WSCK',
+      value: jdwsck,
+      id: wseid,
+      remarks,
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+module.exports.delWSCKEnv = async (wseid) => {
+  const token = await getToken();
+  const body = await api({
+    method: 'delete',
+    url: 'api/envs',
+    params: { t: Date.now() },
+    body: JSON.stringify([wseid]),
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  }).json();
+  return body;
+};
+
+//////////////////////////////////////////////////
